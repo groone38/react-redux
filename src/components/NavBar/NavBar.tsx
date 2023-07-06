19f7d932cddc7c853ae0c0a6122d60313533b4d9
groone38 <groone38@yandex.ru>
2023-07-06 18:38:22 +0300

    fix bag

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -21,28 +21,6 @@ const NavBar = () => {
           <img src={logo} alt="store" className="h-28 w-full" />
         </div>
         <div className="flex flex-row items-center">
-          <button className="font-inter font-medium tracking-normal leading-none text-center mr-4">
-            Logout
-          </button>
-          <div className="flex flex-row items-center">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth="2"
-              stroke="#000"
-              className="w-6 h-6"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z"
-              />
-            </svg>
-            <p className="font-inter font-medium tracking-normal leading-none text-center mr-2">
-              Whish List
-            </p>
-          </div>
           <div
             className="flex flex-row items-center cursor-pointer"
             onClick={handleOpen}
